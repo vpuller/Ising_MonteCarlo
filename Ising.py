342aebad9a0214aec0941347f5c525072a5a55dd
vadim <vpuller@gmail.com>
2020-04-26 15:00:15 +0200

    Working Ising MC for zero H

--- Ising.py
+++ Ising.py
@@ -5,9 +5,10 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from time import time
+import sys
 
-import seaborn as sns
-sns.set()
+# import seaborn as sns
+# sns.set()
 
 np.random.seed()
 
@@ -31,7 +32,7 @@ def sites_loop(S_old, E_old, T):
             # print (np.exp(-(E_new - E_old)/T), p, E_old)
     return S_old, E_old
 
-def run_MC(Lx, Ly, T, Nit = 10**4, S_init = None):
+def run_MC(Lx, Ly, T, Nit = 10**4, S_init = None, Nburn = 10**2):
     if S_init is None:
         #Initializing lattice
         S_init = np.ones((Lx, Ly))
@@ -42,27 +43,47 @@ def run_MC(Lx, Ly, T, Nit = 10**4, S_init = None):
     E_old = energy(S_old)
     M_old = np.sum(S_old)
 
-    EE = [E_old]
-    MM = [M_old]
-    SS = [S_old]
-    for n in range(Nit*Lx*Ly):
-        ix = np.random.randint(Lx)
-        iy = np.random.randint(Ly)
-        p = np.random.rand()
+    # EE = [E_old]
+    # MM = [M_old]
+    # SS = [S_old]
+    Esum = 0
+    E2sum = 0
+    Msum = 0
+    Mabs_sum = 0
+    M2sum = 0
+    Ssum = np.zeros(S_init.shape)
 
+    N = Nit*Lx*Ly
+    iix = np.random.randint(0, high = Lx, size = N)
+    iiy = np.random.randint(0, high = Ly, size = N)
+    pp = np.random.rand(N)
+    # for n in range(Nit*Lx*Ly):
+    k = 0
+    for n, (ix, iy, p) in enumerate(zip(iix, iiy, pp)): 
         S_new = np.copy(S_old)
         S_new[ix,iy] = - S_new[ix,iy]
         E_new = energy(S_new)
 
-        if np.exp(-(E_new - E_old)) > p:
+        if np.exp(-(E_new - E_old)/T) > p:
+            if E_new > E_old:
+                k +=1
             S_old = S_new
             E_old = E_new
 
-        EE.append(E_old)
-        MM.append(np.sum(S_old))
-        SS.append(S_old)
-
-    return np.array(EE), np.array(MM), np.array(SS)
+        if n > Nburn:
+            Esum += E_old
+            E2sum += E_old**2
+            M = np.sum(S_old)
+            Msum += M
+            M2sum += M**2
+            Mabs_sum += np.abs(M)
+            Ssum += S_old
+            # EE.append(E_old)
+            # MM.append(np.sum(S_old))
+            # SS.append(S_old)
+    print(k, N, T)
+    # return np.array(EE), np.array(MM), np.array(SS)
+    return Esum/N, E2sum/N, Msum/N, M2sum/N, Mabs_sum/N, Ssum/N, S_init, S_old
 
 if __name__ == "__main__":
     '''
@@ -76,95 +97,46 @@ if __name__ == "__main__":
     T = 1.
     Nit = Lx*Ly*10**2
 
-    # #Initializing lattice
-    # S_old = np.ones((Lx, Ly))
-    # S_old[np.random.rand(Lx, Ly) > 0.5] = -1
-    # E_old = energy(S_old)
-    # print(S_old)
-    # print(energy(S_old))
+    # t0 = time()
+    # # EE, MM, SS = run_MC(Lx, Ly, T, Nit = 10**5)
+    # E_mean, E2_mean, M_mean, M2_mean, Mabs_mean, S_mean, S_init, S_final = run_MC(Lx, Ly, T, Nit = 10**2, Nburn = 0)
+    # t1 = time()
+    # print(f'simulation time: {t1 - t0}')
 
+    # # NN = np.arange(len(EE))
+    # # fig, axs = plt.subplots(2, 1, figsize = (12, 6))
+    # # axs[0].plot(NN, EE/N)
+    # # axs[0].set_ylabel('Energy/site')
+    # # axs[1].plot(NN, MM/N)
+    # # axs[1].set_ylabel('Magnetization/site')
+    # # for ax in axs:
+    # #     ax.set_xlabel('Iterations')
+    # # plt.tight_layout()
+    # # plt.savefig('./figs/evol.jpg')
+    # # plt.close()
 
-    # #looping over sites
-    # iix = np.random.randint(0, high = Lx, size = N)
-    # iiy = np.random.randint(0, high = Ly, size = N)
-    # pp = np.random.rand(N)
-    # S_new = np.copy(S_old)
-    # for ix, iy, p in zip(iix, iiy, pp):
-    #     # S_new = np.copy(S_old)
-    #     S_new[ix,iy] = - S_new[ix,iy]
-    #     E_new = energy(S_new)
-    #     print ("\n", E_old, E_new, (E_new - E_old)/T, np.exp(-(E_new - E_old)/T), p)
-    #     if np.exp(-(E_new - E_old)/T) > p:
-    #         S_old = S_new
-    #         E_old = E_new
-    #         print (np.exp(-(E_new - E_old)/T), p, E_old)
+    # fig, axs = plt.subplots(1, 3, figsize = (12, 6))
+    # im0 = axs[0].imshow(S_init.T, origin = 'lower', cmap = 'bwr', vmin = -1., vmax = 1.)
+    # im1 = axs[1].imshow(S_final.T, origin = 'lower', cmap = 'bwr', vmin = -1., vmax = 1.)
+    # im2 = axs[2].imshow(S_mean.T, origin = 'lower', cmap = 'bwr', vmin = -1., vmax = 1.)
+    # # fig.colorbar(im1, ax = axs[0])
+    # axs[0].set_title('Initial configuration')
+    # axs[1].set_title('Final configuration')
+    # for ax in axs:
+    #     ax.set_xticks(range(Lx))
+    #     # ax.set_xticklabels(self.labels_iso)
+    #     ax.set_yticks(range(Ly))
+    #     # ax.set_yticklabels(self.labels_frag)    
+    # plt.savefig('./figs/init.jpg')
+    # plt.close()
 
-    # EE = [E_old]
-    # MM = [np.sum(S_old)]
-    # SS = [S_old]
-    # for n in range(Nit):
-    #     S_old, E_old = sites_loop(S_old, E_old, T = .1)
-    #     EE.append(E_old)
-    #     MM.append(np.sum(S_old))
-    #     SS.append(S_old)
+    # sys.exit()
 
-    # print(S_old)
-    # print(energy(S_old))    
-    # EE = np.array(EE)
-    # MM = np.array(MM)
-
-    # #One loop approach
-    # for j in range(Nit):
-    #     ix = np.random.randint(Lx)
-    #     iy = np.random.randint(Ly)
-    #     p = np.random.rand()
-
-    #     S_new = np.copy(S_old)
-    #     S_new[ix,iy] = - S_new[ix,iy]
-    #     E_new = energy(S_new)
-
-    #     if np.exp(-(E_new - E_old)) > p:
-    #         S_old = S_new
-    #         E_old = E_new
-
-    #     EE.append(E_old)
-    #     MM.append(np.sum(S_old))
-    #     SS.append(S_old)
-
-    # print(S_old)
-    # print(energy(S_old), MM[-1])
-    # EE = np.array(EE)
-    # MM = np.array(MM)
-
-    EE, MM, SS = run_MC(Lx, Ly, T, Nit = 10**4)
-
-    NN = np.arange(len(EE))
-    fig, axs = plt.subplots(2, 1, figsize = (12, 6))
-    axs[0].plot(NN, EE/N)
-    axs[0].set_ylabel('Energy/site')
-    axs[1].plot(NN, MM/N)
-    axs[1].set_ylabel('Magnetization/site')
-    for ax in axs:
-        ax.set_xlabel('Iterations')
-    plt.tight_layout()
-    plt.savefig('./figs/evol.jpg')
-    plt.close()
-
-    fig, ax = plt.subplots(1, 1, figsize = (6,6))
-    im = ax.imshow(SS[-1].T, origin = 'lower', cmap = 'Greens')
-    fig.colorbar(im, ax = ax)
-    ax.set_title('Initial configuration')
-    ax.set_xticks(range(Lx))
-    # ax.set_xticklabels(self.labels_iso)
-    ax.set_yticks(range(Ly))
-    # ax.set_yticklabels(self.labels_frag)    
-    plt.savefig('./figs/init.jpg')
-    plt.close()
-
-
+    t0 = time()
     Tmin = 0.01
     Tmax = 10.
-    TT = np.linspace(Tmin, Tmax, 11)
+    TT = np.linspace(Tmin, Tmax, 51)
+    # TT = np.array([.01, .1, 1., 10., 100., 1000., 10000.])
     Nburn = 100
     E_T = np.array([])
     E2_T = np.array([])
@@ -173,31 +145,35 @@ if __name__ == "__main__":
     Mabs_T = np.array([])
     for jT, T in enumerate(TT):
         print(f'{jT}, T = {T}')
-        t0 = time()
-        EE, MM, SS = run_MC(Lx, Ly, T, Nit = 10**4)
         t1 = time()
-        print(f'simulation time: {t1-t0}\n')
-        E = EE[Nburn:]
-        M = MM[Nburn:]
-        E_T = np.append(E_T, E.mean())
-        E2_T = np.append(E2_T, (E**2).mean())
-        M_T = np.append(M_T, M.mean())
-        M2_T = np.append(M2_T, (M**2).mean())
-        Mabs_T = np.append(Mabs_T, np.abs(M).mean())
+        # EE, MM, SS = run_MC(Lx, Ly, T, Nit = 10**4)
+        E_mean, E2_mean, M_mean, M2_mean, Mabs_mean, _, _, _ = run_MC(Lx, Ly, T, Nit = 10**4, Nburn = 10**3)
+        t2 = time()
+        print(f'simulation time: {t2-t1}\n')
+        E_T = np.append(E_T, E_mean)
+        E2_T = np.append(E2_T, E2_mean)
+        M_T = np.append(M_T, M_mean)
+        M2_T = np.append(M2_T, M2_mean)
+        Mabs_T = np.append(Mabs_T, Mabs_mean)
 
-    fig, axs = plt.subplots(5, 1, figsize = (12, 30))
-    axs[0].plot(TT, E_T/N)
-    axs[0].set_ylabel('E')
-    axs[1].plot(TT, (E2_T - E_T**2)/N)
-    axs[1].set_ylabel('var(E)')
-    axs[2].plot(TT, M_T/N)
-    axs[2].set_ylabel('M')
-    axs[3].plot(TT, Mabs_T/N)
-    axs[3].set_ylabel('|M|')
-    axs[4].plot(TT, (M2_T - M_T**2)/N)
-    axs[4].set_ylabel('var(M)')
+    t3 = time()
+    print(f'simulation time: {t3-t0}\n')
+    fig, axs = plt.subplots(3, 2, figsize = (18, 12))
+    axs[0, 0].plot(TT, E_T/N)
+    axs[0, 0].set_ylabel('E')
+    axs[0, 1].plot(TT, (E2_T - E_T**2)/N)
+    axs[0, 1].set_ylabel('var(E)')
+    axs[1, 0].plot(TT, M_T/N)
+    axs[1, 0].set_ylabel('M')
+    axs[1, 1].plot(TT, (M2_T - M_T**2)/N)
+    axs[1, 1].set_ylabel('var(M)')
+    axs[2, 0].plot(TT, Mabs_T/N)
+    axs[2, 0].set_ylabel('|M|')
+    axs[2, 1].plot(TT, (M2_T - Mabs_T**2)/N)
+    axs[2, 1].set_ylabel('var(|M|)')
     for ax in axs:
-        ax.set_xlabel('T')
+        ax[0].set_xlabel('T')
+        ax[1].set_xlabel('T')
     plt.tight_layout()
     plt.savefig('./figs/Temperature.jpg')
     plt.close()
